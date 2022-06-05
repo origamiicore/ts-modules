e09c1ee5c4404dc49e07e9fc7bdc460926895e8b
vahid <vahid.hossaini@gmail.com>
2022-06-05 11:04:10 +0430

    fix bug

---
+++ tsoricaptcha/index.ts
@@ -0,0 +1,9 @@
+import TsoriCptcha from "./src";
+import CaptchaConfig from "./src/models/cpatchaConfig";
+import SimpleDriverConfig from "./src/models/simpleDriverConfig";
+
+export {
+    CaptchaConfig,
+    SimpleDriverConfig
+}
+export default TsoriCptcha;
