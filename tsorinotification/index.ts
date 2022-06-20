7d4db65231569b5e6cc7fbbf840c07874806ba97
vahid <vahid.hossaini@gmail.com>
2022-06-20 17:12:29 +0430

    add refrence

--- tsorinotification/index.ts
+++ tsorinotification/index.ts
@@ -1,6 +1,7 @@
 import TsOriNotification from "./src";
 import EmailConfig from "./src/models/emailConfig";
 import NotificationConfig from "./src/models/notificationConfig";
+import TelegramConfig from "./src/models/telegramConfig";
 import WebServiceConfig from "./src/models/webServiceConfig";
 import NotificationRouter from "./src/notificationRouter";
 
@@ -8,6 +9,7 @@ export {
     NotificationConfig,
     EmailConfig,
     WebServiceConfig,
-    NotificationRouter
+    NotificationRouter,
+    TelegramConfig
 };
 export default TsOriNotification;
