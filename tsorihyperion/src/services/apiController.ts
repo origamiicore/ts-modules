1c51a8b4db176ddcd5fddc5f496101f49252cb95
vahid <vahid.hossaini@gmail.com>
2022-07-09 17:44:05 +0430

    add function

--- tsorihyperion/src/services/apiController.ts
+++ tsorihyperion/src/services/apiController.ts
@@ -8,4 +8,10 @@ export default class ApiController
         var data =await WebService.post(server+'/v1/chain/get_table_rows',req,{},null);
         return new ApiResponse(data,cls);
     }
+    static async getTableByScpe<T>(server:string,req:ApiRequest,cls: { new(data:any): T }):Promise<ApiResponse<T>>
+    {
+        delete req.scope;
+        var data =await WebService.post(server+'/v1/chain/get_table_by_scope',req,{},null);
+        return new ApiResponse(data,cls);
+    }
 }
