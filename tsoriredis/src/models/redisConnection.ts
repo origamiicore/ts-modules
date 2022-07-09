c883296c4b76947eb41b68d5d0ba7887aaabc0b2
vahid <vahid.hossaini@gmail.com>
2022-07-09 17:46:28 +0430

    nullable bug

--- tsoriredis/src/models/redisConnection.ts
+++ tsoriredis/src/models/redisConnection.ts
@@ -9,9 +9,9 @@ export default class RedisConnection
             host?:string
             db?:number
         }) { 
-        if(!fields.port)fields.port=6379;
-        if(!fields.db)fields.db=0;
-        if(!fields.host)fields.host='localhost';
+        if(!fields?.port)fields.port=6379;
+        if(!fields?.db)fields.db=0;
+        if(!fields?.host)fields.host='localhost';
         if (fields) Object.assign(this, fields);
     }
 }
